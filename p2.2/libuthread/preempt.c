54bfa13bfa96c75c354487130d0b69f026c64616
Rahul Padhi <rahulpadhi678@gmail.com>
2025-05-15 17:02:28 -0700

    Finished Phase 3 and 4 - with bugs

--- p2.2/libuthread/preempt.c
+++ p2.2/libuthread/preempt.c
@@ -8,29 +8,77 @@
 
 #include "private.h"
 #include "uthread.h"
-
-/*
- * Frequency of preemption
- * 100Hz is 100 times per second
- */
 #define HZ 100
+
+static struct sigaction old_action;
+static struct itimerval old_timer;
+static bool preemption_enabled = false;
+
+static void preempt_handler(int sig)
+{
+	(void)sig;
+	uthread_yield();
+}
 
 void preempt_disable(void)
 {
-	/* TODO Phase 4 */
+	sigset_t mask;
+	
+	sigemptyset(&mask);
+	sigaddset(&mask, SIGVTALRM);
+	sigprocmask(SIG_BLOCK, &mask, NULL);
 }
 
 void preempt_enable(void)
 {
-	/* TODO Phase 4 */
+	sigset_t mask;
+	
+	sigemptyset(&mask);
+	sigaddset(&mask, SIGVTALRM);
+	sigprocmask(SIG_UNBLOCK, &mask, NULL);
 }
 
-// void preempt_start(bool preempt)
-// {
-// 	/* TODO Phase 4 */
-// }
+void preempt_start(bool preempt)
+{
+	if (!preempt)
+		return;
+
+	/* Instll signal handler for SIGVTALRM */
+	struct sigaction action;
+	action.sa_handler = preempt_handler;
+	sigemptyset(&action.sa_mask);
+	action.sa_flags = 0;
+	
+	if (sigaction(SIGVTALRM, &action, &old_action) < 0) {
+		perror("sigaction");
+		exit(1);
+	}
+
+	/* Configure timer to fire 100 times per second */
+	struct itimerval timer;
+	timer.it_value.tv_sec = 0;
+	timer.it_value.tv_usec = 1000000 / HZ; /* 10000 microseconds = 1/100 second */
+	timer.it_interval.tv_sec = 0;
+	timer.it_interval.tv_usec = 1000000 / HZ;
+
+	if (setitimer(ITIMER_VIRTUAL, &timer, &old_timer) < 0) {
+		perror("setitimer");
+		exit(1);
+	}
+
+	preemption_enabled = true;
+}
 
 void preempt_stop(void)
 {
-	/* TODO Phase 4 */
+	if (!preemption_enabled)
+		return;
+
+	/* Restoring old timer */
+	setitimer(ITIMER_VIRTUAL, &old_timer, NULL);
+
+	/* Restoring old signal action */
+	sigaction(SIGVTALRM, &old_action, NULL);
+
+	preemption_enabled = false;
 }
--- p2.2/libuthread/sem.c
+++ p2.2/libuthread/sem.c
@@ -6,26 +6,77 @@
 #include "sem.h"
 
 struct semaphore {
-	/* TODO Phase 3 */
+	int count;
+	queue_t blocked_queue;
 };
 
 sem_t sem_create(size_t count)
 {
-	/* TODO Phase 3 */
+	struct semaphore *sem = malloc(sizeof(struct semaphore));
+	if (!sem)
+		return NULL;
+
+	sem->count = count;
+	sem->blocked_queue = queue_create();
+	if (!sem->blocked_queue) {
+		free(sem);
+		return NULL;
+	}
+
+	return sem;
 }
 
 int sem_destroy(sem_t sem)
 {
-	/* TODO Phase 3 */
+	if (!sem)
+		return -1;
+
+	/* Cannot destroy if threads are waiting */
+	if (queue_length(sem->blocked_queue) > 0)
+		return -1;
+
+	queue_destroy(sem->blocked_queue);
+	free(sem);
+	return 0;
 }
 
 int sem_down(sem_t sem)
 {
-	/* TODO Phase 3 */
+	if (!sem)
+		return -1;
+
+	/* If resources available, take one */
+	if (sem->count > 0) {
+		sem->count--;
+		return 0;
+	}
+
+	/* No resources available, block current thread */
+	struct uthread_tcb *current = uthread_current();
+	queue_enqueue(sem->blocked_queue, current);
+	uthread_block();
+
+	/* When we wake up, the resource should be available
+	 * But we need to handle the corner case where another thread
+	 * might have taken it before we got scheduled */
+	return 0;
 }
 
 int sem_up(sem_t sem)
 {
-	/* TODO Phase 3 */
+	if (!sem)
+		return -1;
+
+	/* If threads are waiting, wake up the first one */
+	if (queue_length(sem->blocked_queue) > 0) {
+		struct uthread_tcb *blocked_thread;
+		queue_dequeue(sem->blocked_queue, (void **)&blocked_thread);
+		uthread_unblock(blocked_thread);
+	} else {
+		/* No threads waiting, just increment count */
+		sem->count++;
+	}
+
+	return 0;
 }
 
